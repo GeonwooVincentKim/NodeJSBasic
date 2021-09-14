a0d9473d1bb670348980ec7067d88a76520dd4bc
GeonwooVincentKim <kdsnop@gmail.com>
2021-09-14 13:24:38 +0900

    [Update] - Updated fileRead.js file

--- nodeJS/fileRead.js
+++ nodeJS/fileRead.js
@@ -1,5 +1,11 @@
 var fileSave = require('fs');
+var testFolder = './data';
 
 fileSave.readFile('sample.txt', 'utf8', function(err, data){
     console.log(data);
+});
+
+
+fileSave.readdir(testFolder, function(error, filelist){
+    console.log(filelist);
 });
