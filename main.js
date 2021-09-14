c548ede3218cefde1324422a0bc2a70fb6870b73
GeonwooVincentKim <kdsnop@gmail.com>
2021-09-14 14:09:06 +0900

    [Feature] - Get File List by using Node.JS

--- main.js
+++ main.js
@@ -1,62 +1,81 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+ 
+var app = http.createServer(function(request,response){
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    var pathname = url.parse(_url, true).pathname;
+    if(pathname === '/'){
+      if(queryData.id === undefined){
+ 
+        fs.readdir('./data', function(error, filelist){
+          var title = 'Welcome';
+          var description = 'Hello, Node.js';
+          var list = '<ul>';
+          var i = 0;
+          
+          while(i < filelist.length){
+            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+            i = i + 1;
+          }
 
-var app = http.createServer(function (request, response) {
-  var _url = request.url;
-  var queryData = url.parse(_url, true).query;
-  var pathName = url.parse(_url, true).pathname;
-  var title = queryData.id;
+          list = list+'</ul>';
 
-  if (_url == '/') {
-    title = 'Welcome';
-  }
+          var template = convertToHTML(title, list, description);
+          response.writeHead(200);
+          response.end(template);
+        })
+ 
+ 
+ 
+      } else {
+        fs.readdir('./data', function(error, filelist){
+          var title = 'Welcome';
+          var description = 'Hello, Node.js';
+          var list = '<ul>';
+          var i = 0;
+          
+          while(i < filelist.length){
+            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+            i = i + 1;
+          }
 
-  if (_url == '/favicon.ico') {
-    return response.writeHead(404);
-  }
+          list = list+'</ul>';
 
+          fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+            var title = queryData.id;
+            var template = convertToHTML(title, list, description);
 
-  if (pathName === '/') {
-    fs.readFile(`data/${queryData.id}`, 'utf8', function (_err, description) {
-      var template = connectToHTML(title, description);
-
-      response.end(template);
-      response.writeHead(200);
-    });
-  } else {
-    fs.readFile(`data/${queryData.id}`, 'utf8', function (_err, description) {
-      var title = queryData.id;
-      var template = connectToHTML(title, description);
-
-      response.writeHead(200);
-      response.end(template);
-    });
-  }
-
-
+            response.writeHead(200);
+            response.end(template);
+          });
+        });
+      }
+    } else {
+      response.writeHead(404);
+      response.end('Not found');
+    }
+ 
+ 
+ 
 });
-
 app.listen(3000);
 
-function connectToHTML(title, description) {
+function convertToHTML(title, list, description) {
   return `
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        <ul>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ul>
-        <h2>${title}</h2>
-        <p>${description}</p>
-      </body>
-      </html>
-      `;
+            <!doctype html>
+            <html>
+            <head>
+              <title>WEB1 - ${title}</title>
+              <meta charset="utf-8">
+            </head>
+            <body>
+              <h1><a href="/">WEB</a></h1>
+              ${list}
+              <h2>${title}</h2>
+              <p>${description}</p>
+            </body>
+            </html>
+            `;
 }
