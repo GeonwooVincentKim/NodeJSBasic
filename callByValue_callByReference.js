abfe75310f2912e8109e530347bc4f59a426b67e
GeonwooVincentKim <kdsnop@gmail.com>
2021-09-24 22:01:05 +0900

    [Feature] - Call By Reference

--- callByValue_callByReference.js
+++ callByValue_callByReference.js
@@ -1,3 +1,4 @@
+console.log("Call By Value");
 console.log("Single Parameter");
 
 function foo(primitive) {
@@ -22,3 +23,34 @@ var x = 10, y = 10;
 
 console.log(getSumValue(x, y));
 console.log(x, y);
+
+console.log();
+console.log("---------------");
+console.log("===============");
+console.log("---------------");
+console.log();
+console.log("Call By Reference");
+
+var changeVal = function(primitive, obj){
+    primitive += 100;
+    obj.name = "Kim";
+    obj.gender = "Female";
+}
+
+/* 
+    num -> Call By Value
+    obj -> Call By Reference 
+*/
+var num = 100;
+var obj = {
+    name: "Lee",
+    gender: "Male"
+};
+
+console.log(num);
+console.log(obj);
+
+changeVal(num, obj);
+
+console.log(num);
+console.log(obj);
