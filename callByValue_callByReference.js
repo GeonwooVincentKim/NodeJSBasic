d7ff80b6448a352450a35c5e6e62e74447b8b133
GeonwooVincentKim <kdsnop@gmail.com>
2021-09-24 21:55:58 +0900

    [Modify] - Multiple Parameter

--- callByValue_callByReference.js
+++ callByValue_callByReference.js
@@ -1,3 +1,5 @@
+console.log("Single Parameter");
+
 function foo(primitive) {
     primitive += 1;
     return primitive;
@@ -7,3 +9,16 @@ var x = 0;
 
 console.log(foo(x)); // 1
 console.log(x);      // 0
+
+console.log("---------------");
+console.log("Multiple Parameter");
+
+var getSumValue = function(first, second){
+    sum = first + second;
+    return sum;
+}
+
+var x = 10, y = 10;
+
+console.log(getSumValue(x, y));
+console.log(x, y);
