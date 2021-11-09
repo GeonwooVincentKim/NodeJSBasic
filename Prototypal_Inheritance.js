9dff8ea9be1a2a78cc60f0deb90fa47b6b06db97
GeonwooVincentKim <kdsnop@gmail.com>
2021-11-09 11:03:51 +0900

    [Feature] - Prototype inheritance explain as the family map

--- Prototypal_Inheritance.js
+++ Prototypal_Inheritance.js
@@ -10,6 +10,10 @@ var Parent = (function(){
     Parent.prototype.setName = function(){
         return this.name;
     }
+    
+    Parent.prototype.sayHi = function(){
+        console.log("HI! " + this.name)
+    }
 
     return Parent;
 }());
@@ -17,6 +21,47 @@ var Parent = (function(){
 let printParent = new Parent("print My Name");
 console.log(printParent);
 
+// Parent -> objectParent
 var objectParent = Object.create(Parent.prototype);
+
 objectParent.name = "Print your Name"
+objectParent.sayHi();
+
 console.log(objectParent);
+console.log(objectParent instanceof Parent);
+
+console.log("");
+console.log("--------------------");
+console.log("");
+
+var Child = (function(){
+    function Child(name){
+        this.name = name;
+    }
+
+    // Parent -> Child
+    Child.prototype = Object.create(Parent.prototype);
+    
+    Child.prototype.sayHi = function(){
+        console.log("Hello!" + this.name);
+    }
+
+    return Child;
+}());
+
+let printChild = new Child("Print my child name");
+console.log(printChild);
+
+// Parent -> Child -> objectChild
+var objectChild = Object.create(Child.prototype);
+
+objectChild.name = " My Child";
+objectChild.sayHi();
+
+console.log(objectChild);
+
+// Like between Man and his parents.
+console.log(objectChild instanceof Child);
+
+// Like between Man and his grandparents.
+console.log(objectChild instanceof Parent);
