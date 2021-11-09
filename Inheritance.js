c0a77cdafe77f0b86f17a3d45238631a89116b1b
GeonwooVincentKim <kdsnop@gmail.com>
2021-11-09 10:44:18 +0900

    [Feature] - Added new instance

--- Inheritance.js
+++ Inheritance.js
@@ -33,9 +33,38 @@ var Child = (function(){
 
     Child.prototype = new Parent();
 
+    // Override Method
+    Child.prototype.sayHi = function(){
+        console.log("안녕하세요! " + this.name);
+    };
+
+    // Created new method in Child Class
+    Child.prototype.sayBye = function(){
+        console.log("안녕히 가세요~ " + this.name);
+    };
+
     return Child;
 }());
 
 var makeChildSimpleInstance = new Child("Grec");
 makeChildSimpleInstance.sayHi();
 console.log(makeChildSimpleInstance);
+
+console.log();
+
+let displayChildSimpleInstance = new Child("VS Code");
+displayChildSimpleInstance.sayHi();
+console.log(displayChildSimpleInstance);
+
+console.log();
+
+var child = new Child("Child");
+console.log(child);
+
+console.log(Child.prototype);
+
+child.sayHi();
+child.sayBye();
+
+console.log(child instanceof Parent);
+console.log(child instanceof Child);
