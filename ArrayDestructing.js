827dc68096aa7014be3a00e4a6aebf6f1d03e7a9
GeonwooVincentKim <kdsnop@gmail.com>
2021-11-11 10:38:58 +0900

    [Update] - Divide as Data by Array Index and Data by entire array

--- ArrayDestructing.js
+++ ArrayDestructing.js
@@ -1,11 +1,11 @@
 var arr = [1, 2, 3];
 
 /* Data by Array Index */
-// var one = arr[0];
-// var two = arr[1];
-// var three = arr[2];
+var one = arr[0];
+var two = arr[1];
+var three = arr[2];
 
-// console.log(one, two, three);
+console.log(one, two, three);
 
 /* Data by Entire Array */
 const [one, two, three] = arr;
