b1d28ce08677ee3c18a65e0980cc40f514a512ff
GeonwooVincentKim <kdsnop@gmail.com>
2021-11-11 10:03:41 +0900

    [Feature] - Created ArrowFunction.js file

---
+++ ArrowFunction.js
@@ -0,0 +1,18 @@
+// var Prefixer = function(prefix){
+//     this.prefix = prefix;
+// }
+function Prefixer(prefix){
+    this.prefix = prefix;
+}
+
+Prefixer.prototype.prefixArray = function(arr){
+    // (A)
+    return arr.map(function (x){
+        return this.prefix + ' ' + x; // (B)
+    });
+}
+
+var pref = Prefixer("Hi");
+
+console.log(pref);
+console.log(pref.prefixArray(['Lee', 'Kim']));
